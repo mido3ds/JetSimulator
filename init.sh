07cc9c4c0ab1ef3417e5324292c904feb17bf10d
Mahmoud Adas <mido3ds@gmail.com>
2019-10-20 22:39:36 +0200

    sfml audio integrated

--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@
 
 mkdir -p build && cd build
 
-conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
+conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan 2>/dev/null
 conan install .. --build missing && cd .. && cmake -S. -Bbuild
