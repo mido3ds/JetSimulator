b69dc56e63d4b86646be79bf12d60da6caf3d222
Mahmoud Adas <mido3ds@gmail.com>
2020-01-10 14:58:31 +0200

    follow the new cmake cli

--- init.sh
+++ init.sh
@@ -3,4 +3,4 @@
 mkdir -p build && cd build
 
 conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan 2>/dev/null
-conan install .. --build missing && cd .. && cmake -S. -Bbuild
+conan install .. --build missing && cd .. && cmake . -Bbuild
