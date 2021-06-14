6211177c559000b66033423ca35467d47fce5606
Mahmoud Adas <mido3ds@gmail.com>
2021-06-15 01:16:03 +0200

    removed conan files

--- init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir -p build && cd build
-
-conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
-conan install .. --build missing && cd .. && cmake -S. -Bbuild
